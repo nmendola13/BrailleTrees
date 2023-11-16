c44a936c1bcf1686df899692032200e28a1e54dd
nmendola13 <144048525+nmendola13@users.noreply.github.com>
2023-11-16 17:36:04 -0600

    Add files via upload
    outline

---
+++ MiniProj8/BitTree.java
@@ -0,0 +1,18 @@
+import java.io.PrintWriter;
+
+public class BitTree {
+    
+    public BitTree(int n) {
+        
+    }
+
+    public String set(String bits, String value) {
+        return bits;
+    }
+
+    public String get(String bits) throws exception {
+
+    }
+
+    public void dump(PrintWriter pen)
+}   
